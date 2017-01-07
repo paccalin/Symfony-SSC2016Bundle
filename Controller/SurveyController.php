82e2350b27fd2ef1d8543a47acbcb04fd3643388
Axel Paccalin <paccalin.axel@gmail.com>
2017-01-07 15:11:03 +0100

    push des premières données

---
+++ Controller/SurveyController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Surveys\SSC2016Bundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SurveyController extends Controller
+{
+
+	/**
+	 * Page d'accueil de l'enquête
+	 *
+	 * @return     <Response>  ( Rendu de la page )
+	 */
+    public function indexAction()
+    {
+    	//Générer les données
+    	$data = array();
+
+    	//effectuer le rendu de la page
+    	$response = $this->render('SurveysSSC2016Bundle:Survey:index-fr.html.twig', $data);
+        
+        //retourner le rendu finit au kernel
+        return $response;
+    }
+
+    /**
+     * Page d'affichage du formulaire
+     *
+     * @return     <Response>  ( Rendu de la page )
+     */
+    public function formAction()
+    {
+    	//Générer les données
+    	$data = array();
+
+    	//effectuer le rendu de la page
+    	$response = $this->render('SurveysSSC2016Bundle:Survey:form-fr.html.twig', $data);
+        
+        //retourner le rendu finit au kernel
+        return $response;
+    }
+}
---
+++ SurveysSSC2016Bundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Surveys\SSC2016Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SurveysSSC2016Bundle extends Bundle
+{
+}
